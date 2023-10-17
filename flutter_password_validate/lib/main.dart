53a1d0d535b667a28402aca06bc608042c97792a
AmirHossein Bayat <amirbayat.dev@gmail.com>
2023-10-17 11:57:40 +0330

    New Version!

--- flutter_password_validate/lib/main.dart
+++ flutter_password_validate/lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pw_validator/flutter_pw_validator.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+//
+// Created by CodeWithFlexZ
+// Tutorials on my YouTube
+//
+//! Instagram
+//! @CodeWithFlexZ
+//
+//? GitHub
+//? AmirBayat0
+//
+//* YouTube
+//* Programming with FlexZ
+//
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +27,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -29,15 +44,6 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController controller = TextEditingController();
   bool success = false;
 
-  
-
-/////////////////////////////////////
-//@CodeWithFlexz on Instagram
-//
-//AmirBayat0 on Github
-//Programming with Flexz on Youtube
-/////////////////////////////////////
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -46,13 +52,16 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           backgroundColor: success ? Colors.green : Colors.red,
           title: Column(
-            children: const [
-              Text("Password Validate"),
+            children: [
+              Text(
+                "Password Validation",
+                style: GoogleFonts.mPlus1(fontWeight: FontWeight.w500),
+              ),
               Text(
                 "@CodeWithFlexZ",
-                style: TextStyle(
+                style: GoogleFonts.mPlus1(
                     color: Colors.white54,
-                    fontSize: 18,
+                    fontSize: 17,
                     fontWeight: FontWeight.w400),
               ),
             ],
@@ -61,77 +70,87 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 12.5),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Icon(
-                  success ? Icons.lock_open : Icons.lock_outline,
-                  color: success ? Colors.green : Colors.red,
-                  size: 130,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 200,
+                    child: success
+                        ? Lottie.network(
+                            "https://lottie.host/b72d482a-8285-41a6-aed9-19ff59a21d4f/2un8X2czPh.json",
+                          )
+                        : Lottie.asset(
+                            "assets/animation_lnt9yuim.json",
+                          ),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 400,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                      child: TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: Color.fromARGB(255, 207, 207, 207),
+                SizedBox(
+                  height: 350,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color:
+                                            Color.fromARGB(255, 207, 207, 207),
+                                        width: 2),
+                                    borderRadius: BorderRadius.circular(5)),
+                                floatingLabelStyle: GoogleFonts.mPlus1(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color:
+                                          success ? Colors.green : Colors.red,
                                       width: 2),
-                                  borderRadius: BorderRadius.circular(5)),
-                              floatingLabelStyle: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: success ? Colors.green : Colors.red,
-                                    width: 2),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              hintText: "Password",
-                              border: const OutlineInputBorder(
-                                  borderSide: BorderSide()))),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    FlutterPwValidator(
-                      controller: controller,
-                      minLength: 8,
-                      uppercaseCharCount: 2,
-                      numericCharCount: 3,
-                      specialCharCount: 1,
-                      normalCharCount: 3,
-                      width: 400,
-                      height: 150,
-                      onSuccess: () {
-                        setState(() {
-                          success = true;
-                        });
-                        print("MATCHED");
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text("Password is matched")));
-                      },
-                      onFail: () {
-                        setState(() {
-                          success = false;
-                        });
-                        print("NOT MATCHED");
-                      },
-                    ),
-                  ],
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                hintText: "Password",
+                                hintStyle:
+                                    GoogleFonts.mPlus1(color: Colors.black),
+                                border: const OutlineInputBorder(
+                                    borderSide: BorderSide()))),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      FlutterPwValidator(
+                        defaultColor: Colors.grey.shade300,
+                        controller: controller,
+                        successColor: Colors.green.shade700,
+                        minLength: 8,
+                        uppercaseCharCount: 2,
+                        numericCharCount: 3,
+                        specialCharCount: 1,
+                        normalCharCount: 3,
+                        width: 400,
+                        height: 150,
+                        onSuccess: () {
+                          setState(() {
+                            success = true;
+                          });
+                        },
+                        onFail: () {
+                          setState(() {
+                            success = false;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
